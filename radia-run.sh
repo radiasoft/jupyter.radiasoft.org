d3c1bc73e1d982c07f068e2bb03c6df1be24d150
robnagler <github@q33.us>
2016-11-07 23:36:03 +0000

    nullglob needed; only install rssynergia in synergia installed in pyenv

--- radia-run.sh
+++ radia-run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #
 # Copy files into ~/jupyter if they don't already exist
+shopt -s nullglob
 for src in jupyter/*; do
     dst=~/jupyter/$(basename "$src")
     if [[ ! -f $dst ]]; then
         cp -a "$src" "$dst"
     fi
 done
+shopt -u nullglob
 
-(
-    # There is no way to list versions so you can use them programmatically
-    for venv in ~/.pyenv/versions/*; do
-        pyenv activate "$(basename "$venv")"
-        pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
-    done
-)
+# There is no way to list versions so you can use them programmatically
+for venv in ~/.pyenv/versions/*; do
+    (
+        pyenv global "$(basename "$venv")"
+        if python -c 'import synergia' >& /dev/null; then
+            pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
+        fi
+    ) || true
+done
