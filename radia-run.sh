b321c933c3e73f3917398b1902e3688bc4d9beb9
robnagler <github@q33.us>
2016-12-30 00:20:27 +0000

    clear $PYENV_VERSION in bash started by terminado

--- radia-run.sh
+++ radia-run.sh
@@ -10,3 +10,16 @@ for src in jupyter/*; do
         cp -a "$src" "$dst"
     fi
 done
+
+# Workaround $PYENV_VERSION being set in
+# radiasoft/containers/radiasoft/beamsim-jupyter/radia-run.sh
+#TODO(robnagler) Need to find a cleaner fix, which will start
+# terminado without $PYENV_VERSION.
+if ! grep -s -q RS_TERMINADO_INIT ~/.post_bivio_bashrc; then
+    cat >> ~/.post_bivio_bashrc <<'EOF'
+if [[ $JPY_USER && $PYENV_VERSION && ! $RS_TERMINADO_INIT ]]; then
+    export RS_TERMINADO_INIT=1
+    unset PYENV_VERSION PYENV_VIRTUAL_ENV
+fi
+EOF
+fi
