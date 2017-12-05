de75bc0237dcd3a593c1c6ac185b5bfb78a50d0a
robnagler <github@q33.us>
2017-12-05 05:11:06 +0000

    fpc: wrong check for post_bivio_bashrc

--- radia-run.sh
+++ radia-run.sh
@@ -11,27 +11,19 @@ for src in jupyter/*; do
     fi
 done
 
-# Workaround $PYENV_VERSION being set in
-# radiasoft/containers/radiasoft/beamsim-jupyter/radia-run.sh
-#TODO(robnagler) Need to find a cleaner fix, which will start
-# terminado without $PYENV_VERSION.
-if ! grep -s -q RS_TERMINADO_INIT ~/.post_bivio_bashrc; then
-    cat >> ~/.post_bivio_bashrc <<'EOF'
-if [[ $JPY_USER && $PYENV_VERSION && -z $RS_TERMINADO_INIT ]]; then
-    export RS_TERMINADO_INIT=1
-    pyenv deactivate >&/dev/null || true
-    unset PYENV_VERSION PYENV_VIRTUAL_ENV VIRTUAL_ENV
-fi
-if [[ $TERM = xterm ]]; then
-    # WORKAROUND: https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
-    export TERM=ansi
-fi
-EOF
-fi
-
 if [[ $(git config --get --global user.name 2>/dev/null) = '<git-user>' ]]; then
     git config --global user.name "$JPY_USER"
     git config --global user.email "$JPY_USER@users.noreply.github.com"
     git config --global credential.helper 'cache --timeout=3600'
     cat /dev/null > ~/.netrc
+fi
+
+# Workaround TERM=xterm https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
+if ! grep -s -q TERM=ansi ~/.post_bivio_bashrc; then
+    cat >> ~/.post_bivio_bashrc <<'EOF'
+if [[ -n $PS1 ]]; then
+    # WORKAROUND: https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
+    export TERM=ansi
+fi
+EOF
 fi
