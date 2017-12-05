510c0592ce5e4c20db0fbecf22067d9388d6b2e1
robnagler <github@q33.us>
2017-12-05 05:04:45 +0000

    fpc: needs to be in post_bivio_bashrc

--- radia-run.sh
+++ radia-run.sh
@@ -22,19 +22,16 @@ if [[ $JPY_USER && $PYENV_VERSION && -z $RS_TERMINADO_INIT ]]; then
     pyenv deactivate >&/dev/null || true
     unset PYENV_VERSION PYENV_VIRTUAL_ENV VIRTUAL_ENV
 fi
-EOF
-fi
-
 if [[ $TERM = xterm ]]; then
     # WORKAROUND: https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
     export TERM=ansi
 fi
+EOF
+fi
+
 if [[ $(git config --get --global user.name 2>/dev/null) = '<git-user>' ]]; then
     git config --global user.name "$JPY_USER"
     git config --global user.email "$JPY_USER@users.noreply.github.com"
     git config --global credential.helper 'cache --timeout=3600'
     cat /dev/null > ~/.netrc
 fi
-
-#TODO(robnagler) remove after beamsim installed from 8/29/17 or later
-rm -f ~/.pre_bivio_bashrc
