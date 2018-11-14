2715d6e493e4265574be3edccc19c1a44b9c4c64
robnagler <github@q33.us>
2018-11-14 23:49:18 +0000

    support newer jupyterhub if JUPYTERHUB_USER exists, use it

--- radia-run.sh
+++ radia-run.sh
@@ -11,9 +11,10 @@ for src in jupyter/*; do
     fi
 done
 
+u=${JUPYTERHUB_USER:-$JPY_USER}
 if [[ $(git config --get --global user.name 2>/dev/null) = '<git-user>' ]]; then
-    git config --global user.name "$JPY_USER"
-    git config --global user.email "$JPY_USER@users.noreply.github.com"
+    git config --global user.name "$u"
+    git config --global user.email "$u@users.noreply.github.com"
     git config --global credential.helper 'cache --timeout=3600'
     cat /dev/null > ~/.netrc
 fi
