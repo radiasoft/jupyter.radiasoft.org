5358d636d58c95cf62ec5223c09a3d3d56980cc6
robnagler <github@q33.us>
2019-05-11 21:14:44 +0000

    remove patches that have been in release for a while

--- radia-run.sh
+++ radia-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Setup
+# Setup jupyter container
 #
 # Copy template files into ~/jupyter if they don't already exist
 shopt -s nullglob
@@ -17,18 +17,4 @@ if [[ $u && $(git config --get --global user.name 2>/dev/null) = '<git-user>' ]]
     git config --global user.email "$u@users.noreply.github.com"
     git config --global credential.helper 'cache --timeout=3600'
     cat /dev/null > ~/.netrc
-fi
-
-if ! grep 'bivio_path_insert.*jupyter/bin' ~/.post_bivio_bashrc >& /dev/null; then
-    echo 'bivio_path_insert ~/jupyter/bin' >> ~/.post_bivio_bashrc
-fi
-
-# Workaround TERM=xterm https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
-if ! grep -s -q TERM=ansi ~/.post_bivio_bashrc; then
-    cat >> ~/.post_bivio_bashrc <<'EOF'
-if [[ -n $PS1 ]]; then
-    # WORKAROUND: https://github.com/radiasoft/jupyter.radiasoft.org/issues/15
-    export TERM=ansi
-fi
-EOF
 fi
